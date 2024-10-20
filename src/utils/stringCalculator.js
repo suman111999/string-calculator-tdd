d302cdd407db73624052c7fed98d17dc76025dec
suman111999 <chaudharysumankumar@gmail.com>
2024-10-20 17:19:25 +0530

    Add test case and impl for two or more numbers

--- src/utils/stringCalculator.js
+++ src/utils/stringCalculator.js
@@ -1,6 +1,9 @@
 export function add(numbers) {
     //if numbers string is empty then return 0
     if (numbers === '') return 0;
-    //if numbers string contains only one number then return the same number
-    return parseInt(numbers, 10)
+    //hanlde two or multiple numbers
+    //split the string by ',' and convert each part to number using map method
+    //then return the sum of all numbers using reduce method
+    const numsArr = numbers.split(',').map(Number);
+    return numsArr.reduce((total, num) => total + num, 0);
 };
--- src/utils/stringCalculator.test.js
+++ src/utils/stringCalculator.test.js
@@ -5,6 +5,18 @@ test('should return 0 for an empty string', () => {
     expect(add('')).toBe(0);
 })
 
+//single number
 test('should return number itself when single number is provided', () => {
     expect(add('7')).toBe(7);
+})
+
+//two numbers
+
+test('should return sum of two numbers', () => {
+    expect(add('3,5')).toBe(8);
+})
+
+//multiple numbers
+test('should return sum of multiple numbers', () => {
+    expect(add('1,2,3,4,5')).toBe(15);
 })
