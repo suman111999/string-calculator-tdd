44964aa81d3e21224fa24999f8405ddd9bade372
suman111999 <chaudharysumankumar@gmail.com>
2024-10-20 17:13:19 +0530

    Add single number test case and impl

--- src/utils/stringCalculator.js
+++ src/utils/stringCalculator.js
@@ -1,4 +1,6 @@
 export function add(numbers) {
     //if numbers string is empty then return 0
     if (numbers === '') return 0;
-}
+    //if numbers string contains only one number then return the same number
+    return parseInt(numbers, 10)
+};
--- src/utils/stringCalculator.test.js
+++ src/utils/stringCalculator.test.js
@@ -4,3 +4,7 @@ import { add } from './stringCalculator';
 test('should return 0 for an empty string', () => {
     expect(add('')).toBe(0);
 })
+
+test('should return number itself when single number is provided', () => {
+    expect(add('7')).toBe(7);
+})
